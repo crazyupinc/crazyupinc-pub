d157912974a0138850ce05739e64c910886f971b
crazyupinc-design <>
2023-06-16 06:10:52 +0000

    Updates

--- _next/static/YId-EceVuBqJqjgfmVu-v/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/f6a9f8e1f89a0dbd.css","static/chunks/pages/index-46206dbc6a548ffa.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
