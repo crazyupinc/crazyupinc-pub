b719900b255ebda433a76a03d7d000d9da8d8ae7
crazyupinc-design <>
2023-06-21 04:15:11 +0000

    Updates

--- _next/static/27vcUwU9EBM1OZ8528hph/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/62bcbdc8f962804b.css","static/chunks/pages/index-07400323f765e9e0.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/7uFLmWor9dGvimFmuQqhs/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/8c8c2e3cca13b581.css","static/chunks/pages/index-1e8ba6c8fe220caa.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
